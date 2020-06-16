36f5d35a68d058af9d23ac43e19e350c0f209395
steeraw <steeraw2019@gmail.com>
2020-06-16 13:04:46 +0300

    Add ifdef

--- thread.cpp
+++ thread.cpp
@@ -2,35 +2,64 @@
 #include <thread>
 #include <time.h>
 #include <cmath>
+#include <mutex>
+
+#ifdef _WIN64
+#include <Windows.h>
+#endif
+
 #define PI 3.14159265
 
 using namespace std;
 
 class CTimer
 {
+    //LARGE_INTEGER t_start, t_end;
+#ifdef linux
     timespec t_start, t_end;
-    long long spended_time;
+#elif _WIN64
+    long long t_start_speed, t_end_speed, t_start_count, t_end_count;
+#endif
+    long long spent_time;
 public:
     void Start()
     {
+#ifdef linux
         clock_gettime(CLOCK_REALTIME, &t_start);
+#elif _WIN64
+        QueryPerformanceFrequency((LARGE_INTEGER*)& t_start_speed);
+  QueryPerformanceCounter((LARGE_INTEGER*)&t_start_count);
+  //QueryPerformanceFrequency((LARGE_INTEGER*)& t_start);
+#endif
         cout << "Counting started" << endl;
     }
     void End()
     {
+#ifdef linux
         clock_gettime(CLOCK_REALTIME, &t_end);
+#elif _WIN64
+        QueryPerformanceFrequency((LARGE_INTEGER*)& t_end_speed);
+  QueryPerformanceCounter((LARGE_INTEGER*)& t_end_count);
+  //QueryPerformanceFrequency((LARGE_INTEGER*)& t_end);
+#endif
         cout << "Counting finished" << endl;
     }
     void Count()
     {
-        spended_time = t_end.tv_nsec - t_start.tv_nsec;
-        spended_time = 1000*(t_end.tv_sec - t_start.tv_sec)+(t_end.tv_nsec - t_start.tv_nsec)/1000000;
-        cout << "Spended " << spended_time << " miliseconds" << endl;
+#ifdef linux
+        spent_time = 1000 * (t_end.tv_sec - t_start.tv_sec) + (t_end.tv_nsec - t_start.tv_nsec) / 1000000;
+#elif _WIN64
+        spent_time = (1000 * t_end_count / t_end_speed) - (1000 * t_start_count / t_start_speed);
+#endif
+        //spent_time = t_end.tv_nsec - t_start.tv_nsec;
+        cout << "Spent " << spent_time << " miliseconds" << endl;
     }
 
 };
+mutex mu;
 void func(int a, int b)
 {
+
 	long double sum = 0;
 	for (a; a <= b; a++)
 	{
@@ -38,60 +67,60 @@ void func(int a, int b)
 		
 		//cout << "Thread ID: " << this_thread::get_id() << "  Current sum:\t" << sum << endl;
 	}
-	cout << "Thread ID: " << this_thread::get_id() << " Sum =\t" << sum << endl;	
+	mu.lock();
+	cout << "Thread ID: " << this_thread::get_id() << " Sum =\t" << sum << endl;
+	mu.unlock();
 }
+
 void func_2_1()
 {
-    func(1,10000000);
-    func(10000000,20000000);
-    func(20000000,30000000);
-    func(30000000,40000000);
+    func(1,100000000);
+    func(100000000,200000000);
+    func(200000000,300000000);
+    func(300000000,400000000);
 }
 
 void func_2_2()
 {
-    func(40000000,50000000);
-    func(50000000,60000000);
-    func(60000000,70000000);
-    func(70000000,80000000);
+    func(400000000,500000000);
+    func(500000000,600000000);
+    func(600000000,700000000);
+    func(700000000,800000000);
 }
 
 void func_4_1()
 {
-    func(1,10000000);
-    func(10000000,20000000);
+    func(1,100000000);
+    func(100000000,200000000);
 }
 void func_4_2()
 {
-    func(20000000,30000000);
-    func(30000000,40000000);
+    func(200000000,300000000);
+    func(300000000,400000000);
 }
 void func_4_3()
 {
-    func(40000000,50000000);
-    func(50000000,60000000);
+    func(400000000,500000000);
+    func(500000000,600000000);
 }
 void func_4_4()
 {
-    func(60000000,70000000);
-    func(70000000,80000000);
+    func(600000000,700000000);
+    func(700000000,800000000);
 }
 int main()
 {
     CTimer timer1,timer2,timer4,timer8;
-    //timespec ts, te;
-    //long int tt;
-    //clock_gettime(CLOCK_REALTIME, &ts);
-    //cout << ts.tv_nsec << endl;
+
     timer1.Start();
-    func(1,10000000);
-    func(10000000,20000000);
-    func(20000000,30000000);
-    func(30000000,40000000);
-    func(40000000,50000000);
-    func(50000000,60000000);
-    func(60000000,70000000);
-    func(70000000,80000000);
+    func(1,100000000);
+    func(100000000,200000000);
+    func(200000000,300000000);
+    func(300000000,400000000);
+    func(400000000,500000000);
+    func(500000000,600000000);
+    func(600000000,700000000);
+    func(700000000,800000000);
     timer1.End();
     timer2.Start();
     thread th_2_1(func_2_1);
@@ -129,28 +158,6 @@ int main()
 
     timer8.End();
 
-
-
-	/*thread th1(func, 1, 100000000);
-	thread th2(func, 10000000, 200000000);
-
-	func(30, 30000000);
-
-	th1.join();
-	th2.join();
-	timer.End();
-	timer.Count();*/
-    //clock_gettime(CLOCK_REALTIME, &te);
-    //cout << ts.tv_nsec << endl;
-    //tt = te.tv_nsec - ts.tv_nsec;
-    //cout << te.tv_nsec - ts.tv_nsec << endl;
-    //tt=1000000000*(te.tv_sec - ts.tv_sec)+(te.tv_nsec - ts.tv_nsec);
-    //cout << tt << endl;
-	//th2.join();
-	//func(0, 1000000);
-	
-	//func(1, 9);
-	//func(3, 11);
 	cout << "1 thread - ";
     timer1.Count();
     cout << endl;
