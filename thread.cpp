212c52736a8d1b9b3494eda73756ba594a9e06eb
steeraw <steeraw2019@gmail.com>
2020-06-17 11:03:24 +0300

    mutex and lock_guard

--- thread.cpp
+++ thread.cpp
@@ -3,6 +3,7 @@
 #include <time.h>
 #include <cmath>
 #include <mutex>
+#include <vector>
 
 #ifdef _WIN64
 #include <Windows.h>
@@ -11,51 +12,74 @@
 #define PI 3.14159265
 
 using namespace std;
-
+#ifdef _WIN64
 class CTimer
 {
     //LARGE_INTEGER t_start, t_end;
-#ifdef linux
-    timespec t_start, t_end;
-#elif _WIN64
     long long t_start_speed, t_end_speed, t_start_count, t_end_count;
-#endif
     long long spent_time;
 public:
     void Start()
     {
-#ifdef linux
-        clock_gettime(CLOCK_REALTIME, &t_start);
-#elif _WIN64
+
         QueryPerformanceFrequency((LARGE_INTEGER*)& t_start_speed);
   QueryPerformanceCounter((LARGE_INTEGER*)&t_start_count);
   //QueryPerformanceFrequency((LARGE_INTEGER*)& t_start);
-#endif
         cout << "Counting started" << endl;
     }
     void End()
     {
-#ifdef linux
-        clock_gettime(CLOCK_REALTIME, &t_end);
-#elif _WIN64
+
         QueryPerformanceFrequency((LARGE_INTEGER*)& t_end_speed);
   QueryPerformanceCounter((LARGE_INTEGER*)& t_end_count);
   //QueryPerformanceFrequency((LARGE_INTEGER*)& t_end);
-#endif
+
         cout << "Counting finished" << endl;
     }
     void Count()
     {
-#ifdef linux
-        spent_time = 1000 * (t_end.tv_sec - t_start.tv_sec) + (t_end.tv_nsec - t_start.tv_nsec) / 1000000;
-#elif _WIN64
+
         spent_time = (1000 * t_end_count / t_end_speed) - (1000 * t_start_count / t_start_speed);
-#endif
+
         //spent_time = t_end.tv_nsec - t_start.tv_nsec;
         cout << "Spent " << spent_time << " miliseconds" << endl;
     }
 
 };
+#endif
+#ifdef linux
+class CTimer
+{
+    //LARGE_INTEGER t_start, t_end;
+
+    timespec t_start, t_end;
+
+    long long spent_time;
+public:
+    void Start()
+    {
+        clock_gettime(CLOCK_REALTIME, &t_start);
+
+        cout << "Counting started" << endl;
+    }
+    void End()
+    {
+
+        clock_gettime(CLOCK_REALTIME, &t_end);
+
+        cout << "Counting finished" << endl;
+    }
+    void Count()
+    {
+        spent_time = 1000 * (t_end.tv_sec - t_start.tv_sec) + (t_end.tv_nsec - t_start.tv_nsec) / 1000000;
+
+        //spent_time = t_end.tv_nsec - t_start.tv_nsec;
+        cout << "Spent " << spent_time << " miliseconds" << endl;
+    }
+
+
+};
+#endif
 mutex mu;
 void func(int a, int b)
 {
@@ -108,9 +132,143 @@ void func_4_4()
     func(600000000,700000000);
     func(700000000,800000000);
 }
+
+vector<int> vect;
+
+bool prime_detect(int n)
+{
+    for (int i = 2; i < n; ++i) {
+        if(n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+void prime_gen(int n)
+{
+    int num, i = 0;
+
+    while (i < n)
+    {
+
+        num = rand() % 10000;
+
+        if(prime_detect(num))
+        {
+            {
+                lock_guard<mutex> guard(mu);
+                //mu.lock();
+                vect.push_back(num);
+                cout << "Thread ID: " << this_thread::get_id() << " Num =\t" << num << endl;
+                //mu.unlock();
+
+                i++;
+            }
+        }
+
+    }
+}
+
 int main()
 {
     CTimer timer1,timer2,timer4,timer8;
+    timer1.Start();
+    prime_gen(100);
+    prime_gen(100);
+    prime_gen(100);
+    prime_gen(100);
+    prime_gen(100);
+    prime_gen(100);
+    prime_gen(100);
+    prime_gen(100);
+    cout << vect.size() << endl;
+    timer1.End();
+    timer8.Start();
+    thread threads_4_1(prime_gen, 100);
+    thread threads_4_2(prime_gen, 100);
+    thread threads_4_3(prime_gen, 100);
+    thread threads_4_4(prime_gen, 100);
+    thread threads_4_5(prime_gen, 100);
+    thread threads_4_6(prime_gen, 100);
+    thread threads_4_7(prime_gen, 100);
+    thread threads_4_8(prime_gen, 100);
+    /*thread threads_4_9(prime_gen, 25);
+    thread threads_4_10(prime_gen, 25);
+    thread threads_4_11(prime_gen, 25);
+    thread threads_4_12(prime_gen, 25);
+    thread threads_4_13(prime_gen, 25);
+    thread threads_4_14(prime_gen, 25);
+    thread threads_4_15(prime_gen, 25);
+    thread threads_4_16(prime_gen, 25);
+    thread threads_4_17(prime_gen, 25);
+    thread threads_4_18(prime_gen, 25);
+    thread threads_4_19(prime_gen, 25);
+    thread threads_4_20(prime_gen, 25);
+    thread threads_4_21(prime_gen, 25);
+    thread threads_4_22(prime_gen, 25);
+    thread threads_4_23(prime_gen, 25);
+    thread threads_4_24(prime_gen, 25);
+    thread threads_4_25(prime_gen, 25);
+    thread threads_4_26(prime_gen, 25);
+    thread threads_4_27(prime_gen, 25);
+    thread threads_4_28(prime_gen, 25);
+    thread threads_4_29(prime_gen, 25);
+    thread threads_4_30(prime_gen, 25);
+    thread threads_4_31(prime_gen, 25);
+    thread threads_4_32(prime_gen, 25);*/
+    threads_4_1.join();
+    threads_4_2.join();
+    threads_4_3.join();
+    threads_4_4.join();
+    threads_4_5.join();
+    threads_4_6.join();
+    threads_4_7.join();
+    threads_4_8.join();
+    /*threads_4_9.join();
+    threads_4_10.join();
+    threads_4_11.join();
+    threads_4_12.join();
+    threads_4_13.join();
+    threads_4_14.join();
+    threads_4_15.join();
+    threads_4_16.join();
+    threads_4_17.join();
+    threads_4_18.join();
+    threads_4_19.join();
+    threads_4_20.join();
+    threads_4_21.join();
+    threads_4_22.join();
+    threads_4_23.join();
+    threads_4_24.join();
+    threads_4_25.join();
+    threads_4_26.join();
+    threads_4_27.join();
+    threads_4_28.join();
+    threads_4_29.join();
+    threads_4_30.join();
+    threads_4_31.join();
+    threads_4_32.join();*/
+    timer8.End();
+
+    cout << vect.size() << endl;
+    for (auto it = vect.begin(); it != vect.end(); ++it)
+    {
+        cout << *it << " ";
+    }
+
+
+
+
+    cout << endl;
+    cout << "1 thread - ";
+    timer1.Count();
+    cout << endl;
+    cout << "8 threads - ";
+    timer8.Count();
+    cout << endl;
+    /*CTimer timer1,timer2,timer4,timer8;
 
     timer1.Start();
     func(1,100000000);
@@ -169,5 +327,5 @@ int main()
     cout << endl;
     cout << "8 threads - ";
     timer8.Count();
-    cout << endl;
+    cout << endl;*/
 }
